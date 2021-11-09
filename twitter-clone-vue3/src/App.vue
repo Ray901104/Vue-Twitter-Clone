ee9752e31c5c5c3dbb1849eed5393ebf8b808891
DESKTOP-TTTEBN8\ip323 <ip323@naver.com>
2021-11-09 23:29:46 +0900

    Sidemenu 마무리

--- twitter-clone-vue3/src/App.vue
+++ twitter-clone-vue3/src/App.vue
@@ -1,64 +1,102 @@
 <template>
-    <div class="flex h-screen">
-        <!-- 1/4만큼 사이드메뉴 -->
-        <div class="w-1/4 mt-5 ml-10">
-        <!-- twitter logo -->
-            <i class="fab fa-twitter fa-fw text-2xl text-primary ml-4"></i>
-            <!-- flex-col을 넣어야 세로로 정렬 -->
-            <div class="flex flex-col items-start">
-                <!-- sidemenu icons -->
-                <div class="hover:text-primary hover:bg-blue-50 px-4 py-2 rounded-full">
-                    <i class="fas fa-home fa-fw text-2xl"></i>
-                    <span class="ml-5 text-lg">홈</span>
-                </div>
-                <div class="hover:text-primary hover:bg-blue-50 px-4 py-2 rounded-full">
-                    <i class="fas fa-hashtag fa-fw text-2xl"></i>
-                    <span class="ml-5 text-lg">탐색하기</span>
-                </div>
-                <div class="hover:text-primary hover:bg-blue-50 px-4 py-2 rounded-full">
-                    <i class="far fa-bell fa-fw text-2xl"></i>
-                    <span class="ml-5 text-lg">알림</span>
-                </div>
-                <div class="hover:text-primary hover:bg-blue-50 px-4 py-2 rounded-full">
-                    <i class="far fa-envelope fa-fw text-2xl"></i>
-                    <span class="ml-5 text-lg">쪽지</span>
-                </div>
-                <div class="hover:text-primary hover:bg-blue-50 px-4 py-2 rounded-full">
-                    <i class="far fa-bookmark fa-fw text-2xl"></i>
-                    <span class="ml-5 text-lg">북마크</span>
-                </div>
-                <div class="hover:text-primary hover:bg-blue-50 px-4 py-2 rounded-full">
-                    <i class="far fa-list-alt fa-fw text-2xl"></i>
-                    <span class="ml-5 text-lg">리스트</span>
-                </div>
-                <div class="hover:text-primary hover:bg-blue-50 px-4 py-2 rounded-full">
-                    <i class="far fa-user fa-fw text-2xl"></i>
-                    <span class="ml-5 text-lg">프로필</span>
-                </div>
-                <div class="hover:text-primary hover:bg-blue-50 px-4 py-2 rounded-full">
-                    <i class="fas fa-ellipsis-h fa-fw text-2xl"></i>
-                    <span class="ml-5 text-lg">더보기</span>
-                </div>
-
-                <!-- tweet button -->
-                <div>
-                    <button class=" mt-3 bg-primary text-white w-60 h-12 rounded-full hover:bg-dark">
-                        Tweet
-                    </button>
-                </div>
-            </div>
+  <div class="flex h-screen">
+    <!-- 1/4만큼 사이드 영역-->
+    <div class="w-1/4 pt-5 ml-10 flex flex-col justify-between border-r border-gray-200">
+    <div class="flex flex-col items-center lg:items-start">
+      <!-- twitter logo -->
+      <i class="fab fa-twitter fa-fw text-2xl text-primary lg:ml-4 mb-3"></i>
+      <!-- flex-col을 넣어야 세로로 정렬 -->
+      <div class="flex flex-col items-start space-y-1">
+        <!-- sidemenu icons -->
+        <div class="hover:text-primary hover:bg-blue-50 px-4 py-2 rounded-full">
+          <i class="fas fa-home fa-fw text-2xl"></i>
+          <span class="ml-5 text-lg hidden lg:inline-block">홈</span>
         </div>
-        <!-- 그 외 나머지 영역  -->
-        <div class="flex-1 bg-red-200"></div>
+        <div class="hover:text-primary hover:bg-blue-50 px-4 py-2 rounded-full">
+          <i class="fas fa-hashtag fa-fw text-2xl"></i>
+          <span class="ml-5 text-lg hidden lg:inline-block">탐색하기</span>
+        </div>
+        <div class="hover:text-primary hover:bg-blue-50 px-4 py-2 rounded-full">
+          <i class="far fa-bell fa-fw text-2xl"></i>
+          <span class="ml-5 text-lg hidden lg:inline-block">알림</span>
+        </div>
+        <div class="hover:text-primary hover:bg-blue-50 px-4 py-2 rounded-full">
+          <i class="far fa-envelope fa-fw text-2xl"></i>
+          <span class="ml-5 text-lg hidden lg:inline-block">쪽지</span>
+        </div>
+        <div class="hover:text-primary hover:bg-blue-50 px-4 py-2 rounded-full">
+          <i class="far fa-bookmark fa-fw text-2xl"></i>
+          <span class="ml-5 text-lg hidden lg:inline-block">북마크</span>
+        </div>
+        <div class="hover:text-primary hover:bg-blue-50 px-4 py-2 rounded-full">
+          <i class="far fa-list-alt fa-fw text-2xl"></i>
+          <span class="ml-5 text-lg hidden lg:inline-block">리스트</span>
+        </div>
+        <div class="hover:text-primary hover:bg-blue-50 px-4 py-2 rounded-full">
+          <i class="far fa-user fa-fw text-2xl"></i>
+          <span class="ml-5 text-lg hidden lg:inline-block">프로필</span>
+        </div>
+        <div class="hover:text-primary hover:bg-blue-50 px-4 py-2 rounded-full">
+          <i class="fas fa-ellipsis-h fa-fw text-2xl"></i>
+          <span class="ml-5 text-lg hidden lg:inline-block">더보기</span>
+        </div>
+      </div>
+        <!-- tweet button -->
+        <div class="w-full lg:pr-3 flex justify-center">
+          <button
+            class="
+              mt-3
+              bg-primary
+              text-white
+              lg:w-full
+              w-12
+              h-12
+              rounded-full
+              hover:bg-dark
+            "
+          >
+            <span class="hidden lg:block">트윗</span>
+            <i class="fas fa-plus lg:hidden"></i>
+          </button>
+      </div>
     </div>
+      <!-- profile button -->
+      <div class="lg:pr-3 mb-3">
+          <button
+            class="
+            hidden
+            lg:flex
+              mt-3
+              px-2
+              py-1
+              w-full
+              h-12
+              rounded-full
+              hover:bg-blue-50
+              items-center
+            "
+          >
+          <!-- 더미 이미지 -->
+            <img src="http://picsum.photos/100" class="w-10 h-10 rounded-full">
+            <div class="lg:ml-2 hidden lg:block">
+              <div class="text-sm font-bold">youngrae.com</div>
+              <div class="text-xs text-gray-500 text-left">@youngrae</div>
+            </div>
+            <!-- ml-auto로 주면 보통 끝으로 간다 -->
+            <i class="ml-auto fas fa-ellipsis-h fa-fw text-sm hidden lg:block"></i>
+          </button>
+          <div class="lg:hidden flex justify-center">
+            <img src="http://picsum.photos/100" class="w-10 h-10 rounded-full cursor-pointer hover:opacity-80">
+          </div>
+      </div>
+    </div>
+    <!-- 그 외 나머지 영역  -->
+    <div class="flex-1"></div>
+  </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
-<style>
-
-</style>
+<style></style>
