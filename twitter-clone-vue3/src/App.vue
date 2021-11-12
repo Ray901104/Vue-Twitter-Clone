02451b597599f8b5adac8c33d9631ee3fda3c284
LAPTOP-S7Q8KMSK\ip323 <ip323@naver.com>
2021-11-13 08:13:10 +0900

    Trends 마무리 및 컴포넌트 분리

--- twitter-clone-vue3/src/App.vue
+++ twitter-clone-vue3/src/App.vue
@@ -1,172 +1,34 @@
 <template>
-  <div class="flex h-screen">
+  <div class="flex h-screen container mx-auto">
     <!-- side section(1/4) -->
-    <div class="w-1/4 pt-5 ml-10 flex flex-col justify-between border-r border-gray-100">
-    <div class="flex flex-col items-center lg:items-start">
-      <!-- twitter logo -->
-      <i class="fab fa-twitter fa-fw text-2xl text-primary lg:ml-4 mb-3"></i>
-      <!-- flex-col을 넣어야 세로로 정렬 -->
-      <div class="flex flex-col items-start space-y-1">
-        <!-- sidemenu icons -->
-        <div class="hover:text-primary hover:bg-blue-50 px-4 py-2 rounded-full">
-          <i class="fas fa-home fa-fw text-2xl"></i>
-          <span class="ml-5 text-lg hidden lg:inline-block">홈</span>
-        </div>
-        <div class="hover:text-primary hover:bg-blue-50 px-4 py-2 rounded-full">
-          <i class="fas fa-hashtag fa-fw text-2xl"></i>
-          <span class="ml-5 text-lg hidden lg:inline-block">탐색하기</span>
-        </div>
-        <div class="hover:text-primary hover:bg-blue-50 px-4 py-2 rounded-full">
-          <i class="far fa-bell fa-fw text-2xl"></i>
-          <span class="ml-5 text-lg hidden lg:inline-block">알림</span>
-        </div>
-        <div class="hover:text-primary hover:bg-blue-50 px-4 py-2 rounded-full">
-          <i class="far fa-envelope fa-fw text-2xl"></i>
-          <span class="ml-5 text-lg hidden lg:inline-block">쪽지</span>
-        </div>
-        <div class="hover:text-primary hover:bg-blue-50 px-4 py-2 rounded-full">
-          <i class="far fa-bookmark fa-fw text-2xl"></i>
-          <span class="ml-5 text-lg hidden lg:inline-block">북마크</span>
-        </div>
-        <div class="hover:text-primary hover:bg-blue-50 px-4 py-2 rounded-full">
-          <i class="far fa-list-alt fa-fw text-2xl"></i>
-          <span class="ml-5 text-lg hidden lg:inline-block">리스트</span>
-        </div>
-        <div class="hover:text-primary hover:bg-blue-50 px-4 py-2 rounded-full">
-          <i class="far fa-user fa-fw text-2xl"></i>
-          <span class="ml-5 text-lg hidden lg:inline-block">프로필</span>
-        </div>
-        <div class="hover:text-primary hover:bg-blue-50 px-4 py-2 rounded-full">
-          <i class="fas fa-ellipsis-h fa-fw text-2xl"></i>
-          <span class="ml-5 text-lg hidden lg:inline-block">더보기</span>
-        </div>
-      </div>
-        <!-- tweet button -->
-        <div class="w-full lg:pr-3 flex justify-center">
-          <button
-            class="
-              mt-3
-              bg-primary
-              text-white
-              lg:w-full
-              w-12
-              h-12
-              rounded-full
-              hover:bg-dark
-            "
-          >
-            <span class="hidden lg:block">트윗</span>
-            <i class="fas fa-plus lg:hidden"></i>
-          </button>
-      </div>
-    </div>
-      <!-- profile button -->
-      <div class="lg:pr-3 mb-3">
-          <button
-            class="
-            hidden
-            lg:flex
-              mt-3
-              px-2
-              py-1
-              w-full
-              h-12
-              rounded-full
-              hover:bg-blue-50
-              items-center
-            "
-          >
-          <!-- 더미 이미지 -->
-            <img src="http://picsum.photos/100" class="w-10 h-10 rounded-full">
-            <div class="lg:ml-2 hidden lg:block">
-              <div class="text-sm font-bold">youngrae.com</div>
-              <div class="text-xs text-gray-500 text-left">@youngrae</div>
-            </div>
-            <!-- ml-auto로 주면 보통 끝으로 간다 -->
-            <i class="ml-auto fas fa-ellipsis-h fa-fw text-sm hidden lg:block"></i>
-          </button>
-          <div class="lg:hidden flex justify-center">
-            <img src="http://picsum.photos/100" class="w-10 h-10 rounded-full cursor-pointer hover:opacity-80">
-          </div>
-      </div>
-    </div>
+    <Side></Side>
     <!-- main & trend section  -->
     <div class="flex-1">
       <div class="flex h-screen">
         <!-- main section -->
-        <div class="flex-1 border-r border-gray-100 overflow-y-auto">
-          <div class="flex flex-col">
-            <!-- page title -->
-            <div class="border-b border-gray-100 px-3 py-2 font-bold text-lg">홈</div>
-            <!-- tweeting section -->
-            <div class="flex px-3 py-3 border-b-8 border-gray-100">
-              <img src="http://picsum.photos/200" class="w-10 h-10 rounded-full hover:opacity-80 cursor-pointer"/>
-              <div class="ml-2 flex-1 flex flex-col">
-                <textarea placeholder="무슨 일이 일어나고 있나요?" class="w-full text-lg font-bold focus:outline-none mb-3 resize-none"></textarea>
-                <div class="text-right">
-                  <button class="bg-primary text-sm font-bold text-white px-4 py-2 rounded-full hover:bg-dark">트윗</button>
-                </div>
-              </div>
-            </div>
-            <!-- tweets -->
-            <div class="flex px-3 py-3 border-b border-gray-100" v-for="tweet in 10" :key=tweet>
-              <img src="http://picsum.photos/200" class="w-10 h-10 rounded-full hover:opacity-80 cursor-pointer"/>
-              <div class="flex-1 flex flex-col ml-3 space-y-1">
-                <div class="text-sm space-x-1">
-                  <span class="font-bold">조국</span>
-                  <span class="text-gray-500 text-xs">@patriamea</span>
-                  <span>·</span>
-                  <span class="text-gray-500 text-xs">7분</span>
-                  </div>
-                <!-- tweet body -->
-                <div>저와 제 딸은 한일병원 인턴 자원 사실을 정청래 의원이나 부인께 알린 적이 없습니다. [MT] 정청래 "조민 '멘탈'에 경의..아내는 약사, 의사 안뽑아" |</div>
-                <!-- tweet actions -->
-                <div class="flex justify-between">
-                  <div class="text-gray-500 hover:bg-blue-50 hover:text-primary rounded-full p-2">
-                  <i class="far fa-comment"></i>
-                  <span class="ml-1 text-sm">1</span>
-                  </div>
-                  <div class="text-gray-500 hover:bg-green-50 hover:text-green-500 rounded-full p-2">
-                  <i class="fas fa-retweet"></i>
-                  <span class="ml-1 text-sm">2</span>
-                  </div>
-                  <div class="text-gray-500 hover:bg-blue-50 hover:text-red-500 rounded-full p-2">
-                  <i class="far fa-heart"></i>
-                  <span class="ml-1 text-sm">3</span>
-                  </div>
-                  <div class="text-gray-500 hover:bg-blue-50 hover:text-primary rounded-full p-2">
-                    <i class="far fa-share-square"></i>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+        <Main></Main>
         <!-- trend section -->
-        <div class="hidden lg:block lg:w-2/5 px-4 py-2">
-          <!-- tweet search input -->
-          <div class="relative mb-5">
-            <input 
-            placeholder="트위터 검색"
-            type="text" class="pl-12 rounded-full w-full p-2 bg-gray-100 text-sm focus:outline-none focus:ring-1 focus:ring-primary focus:bg-white">
-            <i class="fas fa-search absolute left-0 mt-2 ml-3 text-sm text-light"></i>
-          </div>
-          <!-- trend for me -->
-          <div class="w-full rounded-xl bg-gray-50 flex flex-col">
-            <div class="flex items-center justify-between px-3 py-1 border-b border-gray-100">
-              <span class="text-lg font-bold">나를 위한 트렌드</span>
-              <i class="text-center fas fa-cog text-lg text-primary hover:bg-blue-50 p-2 rounded-full"></i>
-            </div>
-            <div class="border-b border-gray-100 hover:bg-gray-100 px-4 py-2">dd</div>
-          </div>
-        </div>
+        <Trends></Trends>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-export default {};
+import Side from './components/Side.vue'
+import Main from './components/Main.vue'
+import Trends from './components/Trends.vue';
+
+export default {
+  components: {
+    Side,
+    Main,
+    Trends,
+  },
+  setup() {
+
+  },
+};
 </script>
 
 <style></style>
