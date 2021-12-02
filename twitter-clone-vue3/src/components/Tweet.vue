d6630395551cf6cb2bdbcd0a7ff601725fb91f6d
LAPTOP-S7Q8KMSK\ip323 <ip323@naver.com>
2021-12-02 23:42:49 +0900

    트윗 모달 팝업

--- twitter-clone-vue3/src/components/Tweet.vue
+++ twitter-clone-vue3/src/components/Tweet.vue
@@ -1,74 +1,32 @@
 <template>
   <!-- tweets -->
-  <div
-      class="
-      flex
-      px-3
-      py-3
-      border-b border-gray-100
-      hover:bg-gray-50
-      cursor-pointer
-    "
-  >
-    <img
-        :src="currentUser.profile_image_url"
-        class="w-10 h-10 rounded-full hover:opacity-80 cursor-pointer"
-    />
-    <div class="flex-1 flex flex-col ml-3 space-y-1">
+  <div class="flex px-3 py-3 border-b border-gray-100 hover:bg-gray-50 cursor-pointer">
+    <img :src="currentUser.profile_image_url" class="w-10 h-10 rounded-full hover:opacity-80 cursor-pointer"/>
+    <div class="ml-3 flex-1 flex flex-col space-y-1">
       <div class="text-sm space-x-1">
         <span class="font-bold">{{ tweet.email }}</span>
-        <span class="text-gray-500 text-xs">{{ tweet.username }}</span>
+        <span class="text-gray-500 text-xs">@{{ tweet.username }}</span>
         <span>·</span>
         <span class="text-gray-500 text-xs">{{ moment(tweet.created_at).fromNow() }}</span>
       </div>
       <!-- tweet body -->
-      <div>
-        {{ tweet.tweet_body }}
-      </div>
+      <div>{{ tweet.tweet_body }}</div>
       <!-- tweet actions -->
       <div class="flex justify-between">
-        <div
-            class="
-            text-gray-500
-            hover:bg-blue-50 hover:text-primary
-            rounded-full
-            p-2
-          "
-        >
-          <i class="far fa-comment"></i>
+        <div class="text-gray-500 hover:text-primary">
+          <i class="far fa-comment hover:bg-blue-50 rounded-full p-2"></i>
           <span class="ml-1 text-sm">{{ tweet.num_comments }}</span>
         </div>
-        <div
-            class="
-            text-gray-500
-            hover:bg-green-50 hover:text-green-500
-            rounded-full
-            p-2
-          "
-        >
-          <i class="fas fa-retweet"></i>
+        <div class="text-gray-500 hover:text-green-500">
+          <i class="fas fa-retweet hover:bg-green-50 rounded-full p-2"></i>
           <span class="ml-1 text-sm">{{ tweet.num_retweets }}</span>
         </div>
-        <div
-            class="
-            text-gray-500
-            hover:bg-blue-50 hover:text-red-500
-            rounded-full
-            p-2
-          "
-        >
-          <i class="far fa-heart"></i>
+        <div class="text-gray-500 hover:text-red-500">
+          <i class="far fa-heart hover:bg-red-50 rounded-full p-2"></i>
           <span class="ml-1 text-sm">{{ tweet.num_likes }}</span>
         </div>
-        <div
-            class="
-            text-gray-500
-            hover:bg-blue-50 hover:text-primary
-            rounded-full
-            p-2
-          "
-        >
-          <i class="far fa-share-square"></i>
+        <div class="text-gray-500 hover:text-primary">
+          <i class="far fa-share-square hover:bg-blue-50 rounded-full p-2"></i>
         </div>
       </div>
     </div>
@@ -76,14 +34,16 @@
 </template>
 
 <script>
-import moment from 'moment';
+import moment from 'moment'
 
 export default {
   props: ['currentUser', 'tweet'],
   setup() {
-    return {moment};
-  }
-};
+    return {
+      moment,
+    }
+  },
+}
 </script>
 
 <style></style>
--- twitter-clone-vue3/src/layouts/DefaultLayout.vue
+++ twitter-clone-vue3/src/layouts/DefaultLayout.vue
@@ -1,148 +1,96 @@
 <template>
   <div class="flex h-screen container mx-auto relative">
-    <!-- side section(1/4) -->
-    <div
-        class="
-        w-20
-        xl:w-1/4
-        pt-5
-        xl:ml-10
-        flex flex-col
-        justify-between
-        border-r border-gray-100
-      "
-    >
+    <!-- side section -->
+    <div class="w-20 xl:w-1/4 pt-5 xl:ml-10 flex flex-col justify-between border-r border-gray-100">
       <div class="flex flex-col items-center xl:items-start">
         <!-- twitter logo -->
-        <i class="fab fa-twitter fa-fw text-2xl text-primary xl:ml-4 mb-3"></i>
-        <!-- flex-col을 넣어야 세로로 정렬 -->
+        <i class="fab fa-twitter text-3xl text-primary xl:ml-4 mb-3"></i>
+        <!-- sidemenu icons -->
         <div class="flex flex-col items-start space-y-1">
-          <!-- sidemenu icons -->
-          <router-link
-              :to="route.path"
-              :class="`
-              hover:text-primary hover:bg-blue-50
-              p-2
-              xl:px-4
-              xl:py-2
-              rounded-full
-              cursor-pointer
-              ${router.currentRoute.value.name === route.name ? 'text-primary' : ''}
-            `"
-              v-for="route in routes"
-              :key="route"
-          >
+          <router-link :to="route.path"
+                       :class="`hover:text-primary hover:bg-blue-50 p-2 xl:px-4 xl:py-2 rounded-full cursor-pointer ${router.currentRoute.value.name == route.name ? 'text-primary' : ''}`"
+                       v-for="route in routes" :key="route">
             <div v-if="route.meta.isMenu">
               <i :class="route.icon"></i>
-              <span class="ml-5 text-xl hidden xl:inline-block">{{
-                  route.title
-                }}</span>
+              <span class="ml-5 text-xl hidden xl:inline-block">{{ route.title }}</span>
             </div>
           </router-link>
         </div>
         <!-- tweet button -->
         <div class="w-full xl:pr-3 flex justify-center">
-          <button
-              class="
-              mt-3
-              bg-primary
-              text-white
-              xl:w-full
-              w-12
-              h-12
-              rounded-full
-              hover:bg-dark
-            "
-          >
+          <button class="mt-3 bg-primary text-white xl:w-full w-12 h-12 rounded-full hover:bg-dark"
+                  @click="showTweetModal = true">
             <span class="hidden xl:block">트윗</span>
             <i class="fas fa-plus xl:hidden"></i>
           </button>
         </div>
       </div>
       <!-- profile button -->
-      <div class="xl:pr-3 mb-3" @click="showProfileDropdown = true">
-        <button
-            class="
-            hidden
-            xl:flex
-            mt-3
-            px-2
-            py-1
-            w-full
-            h-12
-            rounded-full
-            hover:bg-blue-50
-            items-center
-          "
-        >
-          <!-- 더미 이미지 -->
+      <div class="xl:pr-3 mb-3 relative" @click="showProfileDropdown = true">
+        <button class="hidden xl:flex mt-3 px-2 py-1 w-full h-12 rounded-full hover:bg-blue-50 items-center">
           <img :src="currentUser.profile_image_url" class="w-10 h-10 rounded-full"/>
           <div class="xl:ml-2 hidden xl:block">
             <div class="text-sm font-bold">{{ currentUser.email }}</div>
             <div class="text-xs text-gray-500 text-left">@{{ currentUser.username }}</div>
           </div>
-          <!-- ml-auto로 주면 보통 끝으로 간다 -->
-          <i
-              class="ml-auto fas fa-ellipsis-h fa-fw text-sm hidden xl:block"
-          ></i>
+          <i class="ml-auto fas fa-ellipsis-h fa-fw text-xs hidden xl:block"></i>
         </button>
         <div class="xl:hidden flex justify-center">
-          <img
-              src="https://picsum.photos/100"
-              class="w-10 h-10 rounded-full cursor-pointer hover:opacity-80"
-          />
+          <img :src="currentUser.profile_image_url" class="w-10 h-10 rounded-full cursor-pointer hover:opacity-80"/>
         </div>
       </div>
     </div>
-    <!-- main & trend section  -->
+    <!-- main section -->
     <div class="flex-1 flex h-screen">
-      <router-view></router-view>
+      <router-view/>
     </div>
     <!-- profile dropdown menu -->
-    <div class="absolute bottom-20 left-10 shadow rounded-lg w-60 bg-white" v-if="showProfileDropdown"
+    <div class="absolute bottom-20 left-12 shadow rounded-lg w-60 bg-white" v-if="showProfileDropdown"
          @click="showProfileDropdown = false">
       <button class="hover:bg-gray-50 border-b border-gray-100 flex p-3 w-full items-center">
         <img :src="currentUser.profile_image_url" class="w-10 h-10 rounded-full"/>
         <div class="ml-2">
           <div class="font-bold text-sm">{{ currentUser.email }}</div>
-          <div class="text-left text-gray-500 text-sm">{{ currentUser.username }}</div>
+          <div class="text-left text-gray-500 text-sm">@{{ currentUser.username }}</div>
         </div>
         <i class="fas fa-check text-primary ml-auto"></i>
       </button>
-      <button class="hover:bg-gray-50 p-3 w-full text-left text-sm" @click="onLogout">
-        {{ currentUser.username }} 계정에서 로그아웃
+      <button class="p-3 hover:bg-gray-50 w-full text-left text-sm" @click="onLogout">@{{ currentUser.username }} 계정에서
+        로그아웃
       </button>
     </div>
+    <!--  tweet modal popup  -->
+    <tweet-modal v-if="showTweetModal" @close-modal="showTweetModal = false"></tweet-modal>
   </div>
 </template>
 
 <script>
-import {ref, onBeforeMount, computed} from "vue";
-import router from "../router";
-import {auth} from "../firebase";
-import store from "../store";
+import {ref, onBeforeMount, computed} from 'vue'
+import router from '../router'
+import {auth} from '../firebase'
+import store from '../store'
+import TweetModal from "../components/TweetModal.vue";
 
 export default {
-  components: {},
+  components: {TweetModal},
   setup() {
-    const routes = ref([]);
-    const showProfileDropdown = ref(false);
+    const routes = ref([])
+    const showProfileDropdown = ref(false)
+    const showTweetModal = ref(false);
 
-    const currentUser = computed(() => store.state.user);
+    const currentUser = computed(() => store.state.user)
 
     const onLogout = async () => {
-      await auth.signOut();
-      store.commit("SET_USER", null);
-      await router.replace('/login');
+      await auth.signOut()
+      store.commit('SET_USER', null)
+      await router.replace('/login')
     }
 
     onBeforeMount(() => {
-      routes.value = router.options.routes.filter(route => route.meta.isMenu === true);
-    });
+      routes.value = router.options.routes.filter((route) => route.meta.isMenu === true)
+    })
 
-    return {routes, showProfileDropdown, onLogout, currentUser, router};
+    return {routes, showProfileDropdown, onLogout, currentUser, router, showTweetModal}
   },
-};
+}
 </script>
-
-<style></style>
