308793a12ddad00505f7d0d8772c14ab0ffcf2da
LAPTOP-S7Q8KMSK\ip323 <ip323@naver.com>
2021-12-04 17:39:05 +0900

    알림 & 쪽지 페이지, 트윗 삭제 기능 추가, firebase 호스팅 추가

--- twitter-clone-vue3/src/components/Tweet.vue
+++ twitter-clone-vue3/src/components/Tweet.vue
@@ -5,11 +5,16 @@
       <img :src="tweet.profile_image_url" class="w-10 h-10 rounded-full hover:opacity-80 cursor-pointer"/>
     </router-link>
     <div class="ml-3 flex-1 flex flex-col space-y-1">
-      <div class="text-sm space-x-1">
-        <span class="font-bold">{{ tweet.email }}</span>
-        <span class="text-gray-500 text-xs">@{{ tweet.username }}</span>
-        <span>·</span>
-        <span class="text-gray-500 text-xs">{{ moment(tweet.created_at).fromNow() }}</span>
+      <div class="text-sm flex justify-between items-center">
+        <div class="space-x-1">
+          <span class="font-bold">{{ tweet.email }}</span>
+          <span class="text-gray-500 text-xs">@{{ tweet.username }}</span>
+          <span>·</span>
+          <span class="text-gray-500 text-xs">{{ moment(tweet.created_at).fromNow() }}</span>
+        </div>
+        <button v-if="currentUser.uid === tweet.uid" @click="onDeleteTweet(tweet)">
+          <i class="fas fa-trash text-red-400 p-2 rounded-full hover:bg-red-50"></i>
+        </button>
       </div>
       <!-- tweet body -->
       <router-link :to="`/tweet/${tweet.id}`">{{ tweet.tweet_body }}</router-link>
@@ -56,6 +61,8 @@ import {ref} from "vue";
 import CommentModal from "./CommentModal.vue";
 import handleRetweet from "../utils/handleRetweet";
 import handleLike from "../utils/handleLike";
+import {TWEET_COLLECTION, COMMENT_COLLECTION, RETWEET_COLLECTION, LIKE_COLLECTION, USER_COLLECTION} from "../firebase";
+import firebase from "firebase/compat/app";
 
 export default {
   components: {CommentModal},
@@ -63,12 +70,42 @@ export default {
   setup() {
     const showCommentModal = ref(false);
 
+    const onDeleteTweet = async (tweet) => {
+      if (confirm("정말 트윗을 삭제하시겠습니까?")) {
+        // 트윗 삭제
+        await TWEET_COLLECTION.doc(tweet.id).delete();
+
+        // 댓글 삭제
+        const commentSnapshot = await COMMENT_COLLECTION.where('from_tweet_id', '==', tweet.id).get();
+        commentSnapshot.docs.forEach((doc) => {
+          doc.ref.delete();
+        });
+
+        // 리트윗 삭제
+        const retweetSnapshot = await RETWEET_COLLECTION.where('from_tweet_id', '==', tweet.id).get();
+        retweetSnapshot.docs.forEach((doc) => {
+          doc.ref.delete();
+        });
+
+        // 좋아요 삭제
+        const likeSnapshot = await LIKE_COLLECTION.where('from_tweet_id', '==', tweet.id).get();
+        likeSnapshot.docs.forEach((doc) => {
+          doc.ref.delete();
+        });
+        // USER_COLLECTION에서 num_tweets 1 감소
+        await USER_COLLECTION.doc(tweet.uid).update({
+          num_tweets: firebase.firestore.FieldValue.increment(-1),
+        })
+      }
+    }
+
 
     return {
       moment,
       showCommentModal,
       handleRetweet,
       handleLike,
+      onDeleteTweet,
     }
   },
 }
--- twitter-clone-vue3/src/pages/Messages.vue
+++ twitter-clone-vue3/src/pages/Messages.vue
@@ -6,67 +6,42 @@
         <div class="p-3 font-bold text-lg border-b border-gray-100 h-14">
           쪽지
         </div>
-        <!-- a chat list -->
+        <!-- a user list -->
         <div
-          class="
-            flex
-            px-3
-            py-4
-            hover:bg-gray-50
-            border-b border-gray-100
-            bg-gray-200
-          "
+            @click="onSelectUser(user)"
+            class="flex items-center cursor-pointer px-3 py-4 hover:bg-gray-50 border-b border-gray-100"
+            v-for="user in users"
+            :key="user.id"
         >
           <img
-            src="http://picsum.photos/200"
-            class="w-10 h-10 rounded-full cursor-pointer mr-2"
+              :src="user.profile_image_url"
+              class="w-10 h-10 rounded-full cursor-pointer mr-2"
           />
-          <div>
-            <div class="flex space-x-2">
-              <div class="font-bold">youngrae.com</div>
-              <div class="text-gray-500">@youngrae</div>
-              <div class="text-gray-500">1월 29일</div>
-            </div>
-            <div class="text-gray-500">메세지</div>
-          </div>
-        </div>
-        <div
-          class="flex px-3 py-4 hover:bg-gray-50 border-b border-gray-100"
-          v-for="message in 5"
-          :key="message"
-        >
-          <img
-            src="http://picsum.photos/200"
-            class="w-10 h-10 rounded-full cursor-pointer mr-2"
-          />
-          <div>
-            <div class="flex space-x-2">
-              <div class="font-bold">youngrae.com</div>
-              <div class="text-gray-500">@youngrae</div>
-              <div class="text-gray-500">1월 29일</div>
-            </div>
-            <div class="text-gray-500">메세지</div>
+          <div class="flex space-x-2">
+            <div class="font-bold">{{ user.email }}</div>
+            <div class="text-gray-500">@{{ user.username }}</div>
+            <div class="text-gray-500">{{ moment(user.created_at).format("M월 DD일") }}</div>
           </div>
         </div>
       </div>
     </div>
     <!-- chatting -->
-    <div class="w-3/5 border-r border-gray-100">
+    <div class="w-3/5 border-r border-gray-100" v-if="selectedUser">
       <div class="flex flex-col h-screen">
         <!-- title -->
         <div class="flex px-3 h-14 items-center border-b border-gray-100">
           <img
-            src="http://picsum.photos/100"
-            class="w-8 h-8 rounded-full mr-2 cursor-pointer"
+              :src="selectedUser.profile_image_url"
+              class="w-8 h-8 rounded-full mr-2 cursor-pointer"
           />
           <div>
-            <div class="font-bold text-lg">youngrae.com</div>
-            <div class="text-sm text-gray-500">@youngrae</div>
+            <div class="font-bold text-lg">{{ selectedUser.email }}</div>
+            <div class="text-sm text-gray-500">@{{ selectedUser.username }}</div>
           </div>
         </div>
         <!-- user info -->
         <div
-          class="
+            class="
             flex flex-col
             justify-center
             items-center
@@ -76,43 +51,49 @@
           "
         >
           <div>
-            <span class="font-bold">youngrae.com</span>
-            <span class="text-gray-500">@youngrae</span>
+            <span class="font-bold">{{ selectedUser.email }}</span>
+            <span class="text-gray-500">@{{ selectedUser.username }}</span>
           </div>
           <div>
-            <span class="font-bold mr-1">28</span
+            <span class="font-bold mr-1">{{ selectedUser.followings.length }}</span
             ><span class="text-gray-500">팔로우 중</span>
-            <span class="font-bold ml-3 mr-1">7</span
+            <span class="font-bold ml-3 mr-1">{{ selectedUser.followers.length }}</span
             ><span class="text-gray-500">팔로워</span>
           </div>
           <div>
             <span class="text-gray-500 mr-1">가입일:</span>
-            <span class="text-gray-500">2011년 10월</span>
+            <span class="text-gray-500">{{ moment(selectedUser.created_at).format('YYYY년 MM월') }}</span>
           </div>
         </div>
         <div class="flex-1 overflow-y-scroll">
-          <!-- chatt bubble : my -->
-          <div class="text-right px-3 py-3">
+
+          <div v-for="message in messages" :key="message.id">
+
+            <!-- chatt bubble : my -->
+            <div class="text-right px-3 py-3" v-if="currentUser.uid === message.from_uid">
             <span class="bg-primary text-white px-4 py-2 rounded-full"
-              >메시지</span
+            >{{ message.message_body }}</span
             >
-            <div class="text-xs text-gray-500 mt-2">
-              2021년 11월 14일 오전 9:36
+              <div class="text-xs text-gray-500 mt-2">
+                {{ moment(message.created_at).fromNow() }}
+              </div>
             </div>
-          </div>
-          <!-- chatt bubble : opponent -->
-          <div class="text-left px-3 py-3" v-for="chat in 20" :key="chat">
-            <span class="bg-gray-100 px-4 py-2 rounded-full">메시지</span>
-            <div class="text-xs text-gray-500 mt-2">
-              2021년 11월 14일 오전 9:36
+            <!-- chatt bubble : opponent -->
+            <div class="text-left px-3 py-3" v-else>
+              <span class="bg-gray-100 px-4 py-2 rounded-full">{{ message.message_body }}</span>
+              <div class="text-xs text-gray-500 mt-2">
+                {{ moment(message.created_at).fromNow() }}
+              </div>
             </div>
           </div>
         </div>
         <!-- chat input -->
         <div class="flex items-center bg-white border-t border-gray-100 sticky">
           <input
-            type="text"
-            class="
+              @keyup.enter="onSendMessage"
+              v-model="messageBody"
+              type="text"
+              class="
               m-2
               px-4
               py-1
@@ -122,11 +103,11 @@
               outline-none
               flex-1
             "
-            placeholder="새 쪽지 작성하기"
+              placeholder="새 쪽지 작성하기"
           />
-          <button class="text-center">
+          <button class="text-center" @click="onSendMessage">
             <i
-              class="
+                class="
                 far
                 fa-paper-plane
                 text-primary text-lg
@@ -141,11 +122,85 @@
         </div>
       </div>
     </div>
+    <div class="w-3/5 border-r border-gray-100 m-5" v-else>
+      <div class="font-bold text-lg">선택된 사용자가 없습니다.</div>
+      <div class="text-gray">사용자를 선택해 주세요.</div>
+    </div>
   </div>
 </template>
 
 <script>
-export default {};
+import {computed, onBeforeMount, ref} from "vue";
+import {USER_COLLECTION, MESSAGE_COLLECTION} from "../firebase";
+import store from "../store";
+import moment from "moment";
+
+export default {
+  setup() {
+    const currentUser = computed(() => store.state.user);
+    const users = ref([]);
+    const selectedUser = ref(null);
+    const messageBody = ref('');
+    const messages = ref([]);
+
+    onBeforeMount(async () => {
+      const snapshot = await USER_COLLECTION.orderBy('created_at', 'desc').get();
+      snapshot.docs.forEach((doc) => {
+        let user = doc.data();
+
+        if (user.email === currentUser.value.email) {
+          return
+        }
+        users.value.push(user);
+      })
+    })
+
+    const onSelectUser = async (user) => {
+      selectedUser.value = user;
+
+      let snapshot = await MESSAGE_COLLECTION
+          .where('from_uid', '==', currentUser.value.uid)
+          .where('to_uid', '==', selectedUser.value.uid)
+          .orderBy('created_at', 'desc').get();
+      messages.value = snapshot.docs.map((doc) => doc.data());
+
+      snapshot = await MESSAGE_COLLECTION
+          .where('to_uid', '==', currentUser.value.uid)
+          .where('from_uid', '==', selectedUser.value.uid)
+          .orderBy('created_at', 'desc').get();
+      snapshot.docs.map((doc) => messages.value.push(doc.data()));
+
+      messages.value = messages.value.sort((a, b) => a.created_at > b.created_at ? 0 : -1);
+    }
+
+    const onSendMessage = async () => {
+      if (!messageBody.value || !selectedUser.value) return;
+
+      const doc = MESSAGE_COLLECTION.doc();
+      let message = {
+        id: doc.id,
+        from_uid: currentUser.value.uid,
+        to_uid: selectedUser.value.uid,
+        message_body: messageBody.value,
+        created_at: Date.now(),
+      }
+      await doc.set(message);
+      messages.value.push(message);
+      messageBody.value = '';
+    }
+
+    return {
+      currentUser,
+      users,
+      moment,
+      selectedUser,
+      onSelectUser,
+      onSendMessage,
+      messageBody,
+      messages,
+    }
+  },
+};
 </script>
 
 <style></style>
